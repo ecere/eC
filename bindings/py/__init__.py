dff90b7432145bac4240102288decae8941069ab
Jerome St-Louis <jerome@ecere.com>
2025-06-21 17:58:28 -0400

    bindings/py: Using os.add_dll_directory() in __init__.py

--- bindings/py/__init__.py
+++ bindings/py/__init__.py
@@ -8,5 +8,7 @@ if sys.platform.startswith('win'):
    ecrtLibPath = os.path.join(script_dir, '..', 'ecrt', 'lib')
    new_path = ecrtLibPath + os.pathsep + original_path + os.pathsep
    os.environ['PATH'] = new_path
+   if hasattr(os, 'add_dll_directory'):
+      os.add_dll_directory(ecrtLibPath)
 
 from .ecrt import *
